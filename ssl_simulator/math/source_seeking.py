606bf4d900fb42c24c895dc0208c5f2aa400f96b
jesusBV20 <92217640+jesusBV20@users.noreply.github.com>
2025-05-09 22:31:19 +0200

    [math] add cetralised calculation of mu (source-seeking)

--- ssl_simulator/math/source_seeking.py
+++ ssl_simulator/math/source_seeking.py
@@ -2,7 +2,8 @@
 """
 
 __all__ = [
-    "L_sigma"
+    "L_sigma",
+    "calc_mu_centralized"
 ]
 
 import numpy as np
@@ -13,8 +14,7 @@ def L_sigma(X, sigma):
     """
     Cetralised L_sigma calculation function (only for numerical validation).
 
-    Attributes
-    ----------
+    Attributes:
         X: numpy array
             (N x 2) matrix of agents position from the centroid
         sigma: numpy array
@@ -29,6 +29,21 @@ def L_sigma(X, sigma):
         D_sqr = np.max(x_norms)
     l_sigma_hat = l_sigma_hat / (N * D_sqr)
 
+    return l_sigma_hat.flatten()
+
+def calc_mu_centralized(X, sigma):
+    """
+    Cetralised mu calculation function (only for numerical validation).
+
+    Attributes:
+        X: numpy array
+            (N x 2) matrix of agents position from the centroid
+        sigma: numpy array
+            (N) vector of simgma_values on each row of X
+    """
+    N = X.shape[0]
+    l_sigma_hat = sigma[:, None].T @ X
+    l_sigma_hat = l_sigma_hat / N 
     return l_sigma_hat.flatten()
 
 #######################################################################################
